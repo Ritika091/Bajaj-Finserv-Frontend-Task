9d64f1c185566577bf99368753ef285efdba63e2
Ritika Saxena <ritikasaxena911@gmail.com>
2023-05-19 19:41:13 +0530

    first commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import './Home.css'
+export default function Home() {
+    const [show,setShow]=useState([]);
+    const fetchData=async()=>{
+        const res=await fetch('https://raw.githubusercontent.com/dixitsoham7/dixitsoham7.github.io/main/index.json');
+        const data=await res.json();
+        console.log(data);
+        setShow(data.employees)
+    }
+    useEffect(()=>{
+        fetchData();
+    },[])
+  return (
+    
+    <div className='Home'>
+        <div className="allemp">
+
+{show.map((data)=>(
+            <div className="card">
+                <h1>{data.name}</h1>
+                <div className="cardsec">
+                    <p>ID-{data.id}</p>
+                    <p>{data.designation}</p>
+                </div>
+                <p>{data.skills}</p>
+                <button className='details'>Show Details</button>
+            </div>
+))}
+        </div>
+    </div>
+  )
+}
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+import './Navbar.css'
+
+export default function Navbar() {
+   
+  return (
+    <div className="Nav">
+        <h1>Employee Details Page</h1>
+    <div className="Search">
+    <div class="container-input">
+    <input type="text" placeholder="Search" name="text" class="input"/>
+  </div>
+  </div>
+  </div>
+  )
+}
